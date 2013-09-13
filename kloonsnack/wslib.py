f5d33c5922bdb8ab9c9506456c4e11fcaf999879
Felix Hamann <nvri@dreadworks.de>
2013-09-13 13:14:47 +0200

    Added the WSConnection class and its _greeting method that handles the handshake.

--- kloonsnack/wslib.py
+++ kloonsnack/wslib.py
@@ -23,7 +23,7 @@ import http
 #   not define a maximum header length for http requests,
 #   most web servers do. This implementation defines the
 #   maximum header size as 8k. The websocket initiation
-#   does not carry a payload and a large headers should
+#   does not carry a payload and a large header should
 #   be very unusual. 8k should be sufficient.
 #
 MAX_HTTP_HEADERSIZE = 2**13
@@ -144,3 +144,99 @@ class HTTPRequest():
 
         self.header = {}
         self._parse_header(header[1:])
+
+
+class WSConnection():
+    """
+        Handles all stages of a WebSocket connection.
+    """
+    def _greeting(self):
+        """
+            Handle the opening handshake.
+            http://tools.ietf.org/html/rfc6455 Section 1.3
+        """
+        # wrap the incoming request into
+        # an http request object
+        req = HTTPRequest(self)
+
+        # check validity as per
+        # rfc 6455, section 4.1
+        if req.version < 11:
+            raise WSRequestError('http version is lower than 1.1')
+
+        # check host header field
+        # as per rfc 3986, sections 3.2.2 & 3.2.3;
+        # luckily urlparse (urllib.parse) is conform
+        # to that specification.
+        key = 'host'
+        if not key in req.header:
+            raise WSRequestError('missing host header field')
+
+        try:
+            url = urlparse.urlparse(req.header[key])
+        except ValueError:
+            raise WSRequestError('invalid host header field')
+
+        # check upgrade header field
+        key = 'upgrade'
+        if key not in req.header:
+            raise WSRequestError('missing upgrade header field')
+        elif req.header[key].lower() != 'websocket':
+            raise WSRequestError('invalid upgrade header field')
+
+        # check connection header field
+        key = 'connection'
+        if key not in req.header:
+            raise WSRequestError('missing connection header field')
+        # TODO firefox sends "keep-alive, Upgrade"
+        elif not req.header[key].lower().endswith('upgrade'):
+            raise WSRequestError('invalid connection header field')
+
+        # check origin header field
+        key = 'origin'
+        if key not in req.header:
+            # TODO Means that it is a non-browser
+            # connection. Not sure how
+            # to handle that at the moment.
+            # Maybe a property can be set to
+            # indicate the lack of this field.
+            pass
+
+        key = 'sec-websocket-version'
+        if key not in req.header:
+            raise WSRequestError('missing sec-websocket-version header field')
+        elif req.header[key] != '13':
+            raise WSRequestError('invalid sec-websocket-version header field')
+
+        # check sec-websocket-key header field
+        key = 'sec-websocket-key'
+        if key not in req.header:
+            raise WSRequestError('no sec-websocket-key header field')
+
+        # craft the response key
+        guid = '258EAFA5-E914-47DA-95CA-C5AB0DC85B11'
+        key = req.header[key] + guid
+        key = hashlib.sha1(key).digest()
+        key = base64.b64encode(key)
+
+        # high five to the client
+        response = []
+        response.append('%s %d %s' % (
+            req.protocol,
+            httplib.SWITCHING_PROTOCOLS,
+            httplib.responses[httplib.SWITCHING_PROTOCOLS]
+        ))
+        headers = map(lambda s: ': '.join(s), {
+            'Upgrade': 'websocket',
+            'Connection': 'Upgrade',
+            'Sec-WebSocket-Accept': key
+        }.items())
+        response.extend(headers)
+
+        response = '\r\n'.join(response) + '\r\n\r\n'
+        print 'sending %s' % response
+        self.send(response)
+        self._state = STATE_OPEN
+
+    def __init__(self):
+        pass
